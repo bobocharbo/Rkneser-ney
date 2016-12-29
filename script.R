01e60595b4f3ade2990f0a16caf1dd6432c137e8
bobocharbo <guillaumecharbonnier@live.fr>
2016-12-29 13:43:49 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,6 +1,6 @@
 ### Set the working directory where the unigram, bigram and trigram are stored 
 
-setwd("/home/bobo/cours/markov/kneser-ney/nlpshiny-master/datas")
+setwd("datas/")
 
 ## Import the datas as unigramDF, bigramDF, trigramDF
 for (file in dir()) load(file)
