19c68b6a95f818039c5e93288fb4f2f0fde3921c
bobocharbo <guillaumecharbonnier@live.fr>
2016-12-29 13:54:01 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,10 +1,21 @@
 ### Set the working directory where the unigram, bigram and trigram are stored 
+main_dir <- getwd()
 
-setwd("datas/")
+
+
+### Set the working directory where the unigram, bigram and trigram are stored 
+if (dir.exists(file.path(main_dir, 'datas'))){
+    setwd("datas/")
+}else {
+    dir.create(file.path(main_dir, 'datas'))
+    setwd(file.path(main_dir, 'datas'))
+}
 
 ## Import the datas as unigramDF, bigramDF, trigramDF
 for (file in dir()) load(file)
-rm(file)
 
-
-
+### 3 DataFrames sont disponibles :
+  # unigramDF : { unigram : (str), freq : (int) }
+  # bigramDF : { bigram : (str), freq : (int), unigram : (str), name : (str) }
+  # trigramDF : { trigram : (str), freq : (int), bigram : (str), name : (str) }
+  
